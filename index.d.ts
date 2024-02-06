7bb5438ffa694c293ad7227ffd3772bf50f12c8a
Fu Yan <beingwrong@live.com>
2024-02-06 17:25:00 +0800

    添加Git信息类型定义

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+export interface IGitInfo {
+    branch: string,
+    commitID: string,
+    commitDate: string,
+    commitAuthor: string,
+    commitMessage: string,
+}
+export default {} as IGitInfo;
